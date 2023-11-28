c07797efae4396c5d3fd09623e3184efbaf86023
Christopher <christopher.cornet@laplateforme.io>
2023-11-28 23:45:45 +0100

    Add all works to the gallery

--- FrontEnd/assets/scripts/index.js
+++ FrontEnd/assets/scripts/index.js
@@ -4,8 +4,41 @@ const url = "http://localhost:5678/api/";
 let getAllWorks = async () => {
     const get_work = await fetch(`${url}works`);
     const data = await get_work.json();
+};
 
-    console.log(data);
+// Add all works to the gallery
+let addAllWorks = async () => {
+    const get_work = await fetch(`${url}works`);
+    const data = await get_work.json();
+
+    let gallery = document.querySelector(".gallery");
+
+    // Create all the images and captions
+    for (let i = 0; i < data.length; i++) {
+        // Get the image and title source
+        let imageSrc = data[i].imageUrl;
+        let titleSrc = data[i].title;
+
+        // Create figure, image and title
+        let figure = document.createElement("figure");
+        let image = document.createElement("img");
+        let title = document.createElement("figcaption");
+
+        // Add the attributes to the image
+        image.src = imageSrc;
+        image.alt = titleSrc;
+
+        // Add the title
+        title.innerHTML = titleSrc;
+
+        // Add figure to the gallery
+        gallery.appendChild(figure);
+
+        // Add image and title to the figure
+        figure.appendChild(image);
+        figure.appendChild(title);
+    }
 };
 
 getAllWorks();
+addAllWorks();
