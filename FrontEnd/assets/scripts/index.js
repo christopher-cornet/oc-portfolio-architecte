ae22c9e1c1669234532c1e1b802f8d40549b3f52
Christopher <christopher.cornet@laplateforme.io>
2023-11-29 23:18:30 +0100

    First version of the Filter function

--- FrontEnd/assets/scripts/index.js
+++ FrontEnd/assets/scripts/index.js
@@ -1,13 +1,10 @@
 const url = "http://localhost:5678/api/";
 
-// Get all works
-let getAllWorks = async () => {
-    const get_work = await fetch(`${url}works`);
-    const data = await get_work.json();
-};
+let all_buttons = document.querySelectorAll(".categories button");
 
 // Add all works to the gallery
 let addAllWorks = async () => {
+    // Get all works
     const get_work = await fetch(`${url}works`);
     const data = await get_work.json();
 
@@ -40,5 +37,36 @@ let addAllWorks = async () => {
     }
 };
 
-getAllWorks();
+// Filter work based on button clicked
+let filterWork = async (filter) => {
+    const get_work = await fetch(`${url}categories`);
+    const data = await get_work.json();
+
+    let buttons = new Set(); // Create a Set
+    buttons.add(filter); // Add the filter number/ID
+
+    // Check which filter to apply
+    if (buttons.has(1)) {
+        console.log("Objets");
+    }
+    else if (buttons.has(2)) {
+        console.log("Appartements");
+    }
+    else if (buttons.has(3)) {
+        console.log("Hotels & restaurants");
+    }
+    else {
+        console.log("Tous");
+    }
+
+}
+
 addAllWorks();
+
+// Each button has an Event Listener. On click, call the filterWork function
+for (let index = 0; index < all_buttons.length; index++) {
+    // Add an event listener on all buttons
+    all_buttons[index].addEventListener("click", () => {
+        filterWork(index); // Give a number (0, 1, 2, or 3) in parameter
+    });
+}
