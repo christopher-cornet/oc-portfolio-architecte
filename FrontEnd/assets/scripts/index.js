4b5606b806ef656e84ab6cdbff29a3ee4e025085
Christopher <christopher.cornet@laplateforme.io>
2023-11-30 17:14:52 +0100

    Filter part is now functional but not optimized

--- FrontEnd/assets/scripts/index.js
+++ FrontEnd/assets/scripts/index.js
@@ -1,71 +1,16 @@
 const url = "http://localhost:5678/api/";
 
 // Add all works to the gallery
-let addAllWorks = async (categoryName) => {
+let addAllWorks = async (filter) => {
+    removeAllWork(); // Remove all the precedent work
+
     // Get all works
     const get_work = await fetch(`${url}works`);
     const data = await get_work.json();
 
     let gallery = document.querySelector(".gallery");
 
-    if (categoryName == "objets") {
-        console.log(categoryName);
-        data.forEach((categorie, index) => {
-            if (categorie.categoryId == 1) {
-                // Get the image and title source
-                let imageSrc = data[index].imageUrl;
-                let titleSrc = data[index].title;
-
-                // Create figure, image and title
-                let figure = document.createElement("figure");
-                let image = document.createElement("img");
-                let title = document.createElement("figcaption");
-
-                // Add the attributes to the image
-                image.src = imageSrc;
-                image.alt = titleSrc;
-
-                // Add the title
-                title.innerHTML = titleSrc;
-
-                // Add figure to the gallery
-                gallery.appendChild(figure);
-
-                // Add image and title to the figure
-                figure.appendChild(image);
-                figure.appendChild(title);
-            }
-        });
-    }
-    else if (categoryName == "appartements") {
-        data.forEach((categorie, index) => {
-            if (categorie.categoryId == 2) {
-                // Get the image and title source
-                let imageSrc = data[index].imageUrl;
-                let titleSrc = data[index].title;
-
-                // Create figure, image and title
-                let figure = document.createElement("figure");
-                let image = document.createElement("img");
-                let title = document.createElement("figcaption");
-
-                // Add the attributes to the image
-                image.src = imageSrc;
-                image.alt = titleSrc;
-
-                // Add the title
-                title.innerHTML = titleSrc;
-
-                // Add figure to the gallery
-                gallery.appendChild(figure);
-
-                // Add image and title to the figure
-                figure.appendChild(image);
-                figure.appendChild(title);
-            }
-        });
-    }
-    else {
+    if (filter == 0) {
         // Create all the images and captions
         for (let i = 0; i < data.length; i++) {
             // Get the image and title source
@@ -91,6 +36,82 @@ let addAllWorks = async (categoryName) => {
             figure.appendChild(image);
             figure.appendChild(title);
         }
+    }
+    else {
+        data.forEach((element) => {
+            if (element.categoryId == 1 && filter == 1) {
+                // Get the image and title source
+                let imageSrc = element.imageUrl;
+                let titleSrc = element.title;
+
+                // Create figure, image and title
+                let figure = document.createElement("figure");
+                let image = document.createElement("img");
+                let title = document.createElement("figcaption");
+
+                // Add the attributes to the image
+                image.src = imageSrc;
+                image.alt = titleSrc;
+
+                // Add the title
+                title.innerHTML = titleSrc;
+
+                // Add figure to the gallery
+                gallery.appendChild(figure);
+
+                // Add image and title to the figure
+                figure.appendChild(image);
+                figure.appendChild(title);
+            }
+            else if (element.categoryId == 2 && filter == 2) {
+                // Get the image and title source
+                let imageSrc = element.imageUrl;
+                let titleSrc = element.title;
+
+                // Create figure, image and title
+                let figure = document.createElement("figure");
+                let image = document.createElement("img");
+                let title = document.createElement("figcaption");
+
+                // Add the attributes to the image
+                image.src = imageSrc;
+                image.alt = titleSrc;
+
+                // Add the title
+                title.innerHTML = titleSrc;
+
+                // Add figure to the gallery
+                gallery.appendChild(figure);
+
+                // Add image and title to the figure
+                figure.appendChild(image);
+                figure.appendChild(title);
+            }
+            else if (element.categoryId == 3 && filter == 3) {
+                // Get the image and title source
+                let imageSrc = element.imageUrl;
+                let titleSrc = element.title;
+
+                // Create figure, image and title
+                let figure = document.createElement("figure");
+                let image = document.createElement("img");
+                let title = document.createElement("figcaption");
+
+                // Add the attributes to the image
+                image.src = imageSrc;
+                image.alt = titleSrc;
+
+                // Add the title
+                title.innerHTML = titleSrc;
+
+                // Add figure to the gallery
+                gallery.appendChild(figure);
+
+                // Add image and title to the figure
+                figure.appendChild(image);
+                figure.appendChild(title);           
+            }
+        });
     }
 };
 
@@ -122,18 +143,6 @@ let addCategories = async () => {
         all_buttons[index].classList.add(categorie.className);
         all_buttons[index].innerHTML = categorie.text;
     });
-
-    // buttons[0].classList.add("all");
-    // buttons[0].innerHTML = "Tous";
-
-    // buttons[1].classList.add("objects");
-    // buttons[1].innerHTML = "Objets";
-
-    // buttons[2].classList.add("appartements");
-    // buttons[2].innerHTML = "Appartements";
-
-    // buttons[3].classList.add("hotels-restaurants");
-    // buttons[3].innerHTML = "Hôtels & restaurants";
 }
 
 let removeAllWork = () => {
@@ -150,7 +159,7 @@ let filterWork = async (filter) => {
     const get_work = await fetch(`${url}works`);
     const data = await get_work.json();
 
-    console.log(data);
+    // console.log(data);
 
     let buttons = new Set(); // Create a Set
     buttons.add(filter); // Add the filter number
@@ -160,39 +169,12 @@ let filterWork = async (filter) => {
         removeAllWork();
     }
 
-    // console.log(filter);
-
-    // // Data Index = Filter - 1
-    // let index = filter - 1;
-
-    // // Check which filter to apply
-    // if (buttons.has(1)) {
-    //     console.log("Nom de la catégorie :", data[index].category.name);
-    //     data.forEach((work, index) => {
-    //         if (work.category.name == "Objets") {
-    //             addAllWorks("objets");
-    //         }
-    //     });
-    // }
-    // else if (buttons.has(2)) {
-    //     console.log("Nom de la catégorie :", data[index].category.name);
-    //     data.forEach((work, index) => {
-    //         if (work.category.name == "Appartements") {
-    //             addAllWorks("appartements");
-    //         }
-    //     });
-    // }
-    // else if (buttons.has(3)) {
-    //     console.log("Nom de la catégorie :", data[index].category.name);
-    // }
-    // else {
-    //     console.log("Tous");
-    // }
+    addAllWorks(filter);
 }
 
 addCategories();
 
-addAllWorks();
+addAllWorks(0);
 
 document.addEventListener("DOMContentLoaded", () => {
     let all_buttons = document.querySelectorAll(".categories button");
